e36c34f3bb8f88f845c7d30819d72a29ba701a1a
SamuelGlezMrt <71700577+SamuelGlezMrt@users.noreply.github.com>
2021-10-13 08:53:19 +0100

    Update BuscarReemplazar.java

--- src/main/java/dad/maven/buscarReemplazar/BuscarReemplazar.java
+++ src/main/java/dad/maven/buscarReemplazar/BuscarReemplazar.java
@@ -1,5 +1,38 @@
 package dad.maven.buscarReemplazar;
 
-public class BuscarReemplazar {
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+public class BuscarReemplazar extends Application {
+
+	private Label BuscarLabel;
+	private Label ReemplazarLabel;
+	private TextField BuscarText;
+	private TextField ReemplazarText;
+	
+	private CheckBox mayusMinusCheckBox;
+	private CheckBox ExpresionCheckBox;
+	private CheckBox BuscarCheckBox;
+	private CheckBox ResaltarCheckBox;
+	
+	private Button BuscarButton;
+	private Button ReemplazarButton;
+	private Button ReemplazarTodoButton;
+	private Button CerrarButton;
+	private Button AyudaButton;
+	
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
